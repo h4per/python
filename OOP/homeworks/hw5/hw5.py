a5df9e26f85db322fca108013f8e42b520bfffde
Islam Segizbaev <100071498+h4per@users.noreply.github.com>
2023-05-25 19:27:48 +0600

    Delete OOP/homeworks/hw5 directory

--- OOP/homeworks/hw5/hw5.py
+++
@@ -1,4 +0,0 @@
-from decouple import config
-
-money = config("MY_MONEY", default="Unknown")
-# print(money)
--- OOP/homeworks/hw5/main.py
+++
@@ -1,5 +0,0 @@
-from game_logic import play_game
-
-def main():
-    play_game()
-main()
