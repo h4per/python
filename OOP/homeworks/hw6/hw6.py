ff5905048361fa065e225b2229cc2a16d12c70bb
h4per <h4peryt@gmail.com>
2023-05-31 01:12:24 +0600

    oop6

--- OOP/homeworks/hw6/hw6.py
+++ OOP/homeworks/hw6/hw6.py
@@ -43,34 +43,30 @@ class Data:
         self.__color = new_color
 
 
-with open("MOCK_DATA.txt", "r", encoding='utf-8') as file:
-    content = file.read()
+with open('MOCK_DATA.txt', 'r', encoding="utf-8") as file_with_names:
+    file = file_with_names.readlines()
+    contents = []
+    for line in file:
+        full_name = re.findall(r"^[A-Za-z'\-]+\s[A-Za-z'\-]+", line)
+        emails = re.findall(r"	[a-zA-Z0-9]+@[-a-zA-Z0-9]+\.[a-zA-Z0-9]+", line)
+        files = re.findall(r"	\w+\.\w+", line)
+        colors = re.findall(r"[#]\w+", line)
+        contents.append(
+            Data(full_name, emails, files, colors)
+        )
 
-    full_name_list = re.findall(r"[A-Za-z'\-]+\s[A-Za-z'\-]+", content)
-    print(full_name_list)
+    with open('full_name', 'w', encoding="utf-8") as file1:
+        for i in contents:
+            file1.writelines(i.full_name)
 
-    email_list = re.findall(r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+", content)
+    with open('email', 'w', encoding="utf-8") as file2:
+        for i in contents:
+            file2.writelines(i.email)
 
-    file_list = re.findall(r"[A-Z]\w+\.\w+", content)
+    with open('file_name', 'w', encoding="utf-8") as file3:
+        for i in contents:
+            file3.writelines(i.file_name)
 
-    color_list = re.findall(r"[#]\w+", content)
-
-fails = [Data(full_name="Nuesfs", email="beka@gmail.com", file_name="beka.py", color="#046220"),
-         Data(full_name="IYAsiudy", email="tguysrc@ehow.com", file_name="UtTellus.tiff", color="#e8a767"),
-         Data(full_name="HSKD", email="Josadfasd@nyu.edu", file_name="PlateaDictumst.txt", color="#ddc1be"),
-         Data(full_name="ASKSD", email="rigonetqasd@mysql.com", file_name="DolorQuis.jpeg", color="aa422c"),
-         Data(full_name="ASKHDSD", email="mk6@sourceforge.net", file_name="SodalesScelerisque.mov", color="#012499")]
-
-for data in fails:
-    if data.full_name in full_name_list:
-        with open("FIO.txt", "w") as f:
-            f.write(data.full_name)
-    if data.email in email_list:
-        with open("email.txt", "w") as f:
-            f.write(data.email)
-    if data.file_name in file_list:
-        with open("filename.txt", "w") as f:
-            f.write(data.file_name)
-    if data.color in color_list:
-        with open("color.txt", "w") as f:
-            f.write(data.color)
+    with open('color', 'w', encoding="utf-8") as file4:
+        for i in contents:
+            file4.writelines(i.color)
